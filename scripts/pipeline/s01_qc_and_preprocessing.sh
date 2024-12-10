c44d724b6d11a1948f3bcd972c3bde8eb88d486c
Matthew Spriggs <matspriggs@gmail.com>
2024-12-10 16:00:27 +0000

    updated s01_qc_and_preprocessing.sh with batch info

--- scripts/pipeline/s01_qc_and_preprocessing.sh
+++ scripts/pipeline/s01_qc_and_preprocessing.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 # ATACseq analysis: QC and Preprocessing
-# Alexey Larionov 25Nov2024
+# Matthew Spriggs 10Dec2024
+
+# PBS directives that you should review and change if needed
+#-----------------------------------------------------------
+
+#PBS -N s01_qc_and_preprocessing
+#PBS -l nodes=1:ncpus=6
+#PBS -l walltime=00:30:00
+#PBS -q half_hour
+#PBS -m abe
+#PBS -M matthew.spriggs.452@cranfield.ac.uk
+
+# PBS directives and code that you should not change
+#===================================================
+#PBS -j oe
+#PBS -v "CUDA_VISIBLE_DEVICES="
+#PBS -W sandbox=PRIVATE
+#PBS -k n
+ln -s $PWD $PBS_O_WORKDIR/$PBS_JOBID
+## Change to working directory
+cd $PBS_O_WORKDIR
+## Calculate number of CPUs and GPUs
+export cpus=`cat $PBS_NODEFILE | wc -l`
+## Load production modules
+module use /apps2/modules/all
+## =============
 
 # Stop at runtime errors
 set -e
@@ -82,3 +107,7 @@ multiqc .
 # Completion message
 echo "Done"
 date
+
+# Clean-up (keep it at the end of the script)
+## ==========================================
+rm $PBS_O_WORKDIR/$PBS_JOBID
