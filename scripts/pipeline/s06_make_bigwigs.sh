c6a32250b00a4a7dcb6c9a7a2f1237c0a1e4d0aa
Matthew Spriggs <matspriggs@gmail.com>
2024-12-10 18:01:50 +0000

    added s06

--- scripts/pipeline/s06_make_bigwigs.sh
+++ scripts/pipeline/s06_make_bigwigs.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 # ATACseq analysis: making BigWigs
-# Alexey Larionov 25Nov2024
+# Matthew Spriggs 10Dec2024
+
+# PBS directives that you should review and change if needed
+#-----------------------------------------------------------
+
+#PBS -N s01_qc_and_preprocessing
+#PBS -l nodes=1:ncpus=6
+#PBS -l walltime=00:30:00
+#PBS -q half_hour
+#PBS -m abe
+#PBS -M matthew.spriggs.453@cranfield.ac.uk
+
+# PBS directives and code that you should not change
+#===================================================
+#PBS -j oe
+#PBS -v "CUDA_VISIBLE_DEVICES="
+#PBS -W sandbox=PRIVATE
+#PBS -k n
+ln -s $PWD $PBS_O_WORKDIR/$PBS_JOBID
+## Change to working directory
+cd $PBS_O_WORKDIR
+## Calculate number of CPUs and GPUs
+export cpus=`cat $PBS_NODEFILE | wc -l`
+## Load production modules
+module use /apps2/modules/all
+## =============
+
 
 # Stop at runtime errors
 set -e
@@ -14,7 +40,7 @@ echo ""
 module load deepTools/3.5.2-foss-2022a # for bamCoverage
 
 # Folders
-base_folder="/mnt/beegfs/home/alexey.larionov/teaching_2024/epigenetics"
+base_folder="/mnt/beegfs/home/s430452/epigenetics"
 samples_folder="${base_folder}/data/atac_seq" # contains samples file
 final_bams_folder="${base_folder}/results/s05_final_bams" # contains BAMs
 bigwigs_folder="${base_folder}/results/s06_bigwigs" # for results
@@ -66,3 +92,7 @@ bamCoverage --bam "${final_bams_folder}/master.bam" \
 echo ""
 echo "Done"
 date
+
+# Clean-up (keep it at the end of the script)
+## ==========================================
+rm $PBS_O_WORKDIR/$PBS_JOBID
