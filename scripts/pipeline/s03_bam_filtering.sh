7ae522d1d4743c84b3233785ad587118e697e0d1
Matthew Spriggs <matspriggs@gmail.com>
2024-12-10 17:12:39 +0000

    updated s03_bam_filtering.sh

--- scripts/pipeline/s03_bam_filtering.sh
+++ scripts/pipeline/s03_bam_filtering.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 # ATACseq analysis: BAMs filtering
-# Alexey Larionov 25Nov2024
+# Matthew Spriggs 10Dec2024
+
+# PBS directives that you should review and change if needed
+#-----------------------------------------------------------
+
+#PBS -N s01_qc_and_preprocessing
+#PBS -l nodes=1:ncpus=6
+#PBS -l walltime=00:30:00
+#PBS -q half_hour
+#PBS -m abe
+#PBS -M matthew.spriggs.453@cranfield.ac.uk
+
+# PBS directives and code that you should not change
+#===================================================
+#PBS -j oe
+#PBS -v "CUDA_VISIBLE_DEVICES="
+#PBS -W sandbox=PRIVATE
+#PBS -k n
+ln -s $PWD $PBS_O_WORKDIR/$PBS_JOBID
+## Change to working directory
+cd $PBS_O_WORKDIR
+## Calculate number of CPUs and GPUs
+export cpus=`cat $PBS_NODEFILE | wc -l`
+## Load production modules
+module use /apps2/modules/all
+## =============
 
 # Stop at runtime errors
 set -e
@@ -14,7 +39,7 @@ echo ""
 module load SAMtools/1.16.1-GCC-11.3.0
 
 # Folders for input and output
-base_folder="/mnt/beegfs/home/alexey.larionov/teaching_2024/epigenetics"
+base_folder="/mnt/beegfs/home/s430452/epigenetics"
 samples_folder="${base_folder}/data/atac_seq" # contains samples file
 raw_bam_folder="${base_folder}/results/s02_alignment" # contains raw bam files
 filtered_bam_folder="${base_folder}/results/s03_filtering" # for results
