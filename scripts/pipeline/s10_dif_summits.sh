cb71a7382f85454705f8c6bdc611cc67e9e21324
Matthew Spriggs <matspriggs@gmail.com>
2024-12-11 14:07:37 +0000

    added s10 scipr for PBS submission

--- scripts/pipeline/s10_dif_summits.sh
+++ scripts/pipeline/s10_dif_summits.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 # ATACseq analysis: selecting summits for peaks with differential coverage
-# Alexey Larionov 25Nov2024
+# Matthew Spriggs 11Dec2024
+
+# PBS directives that you should review and change if needed
+#-----------------------------------------------------------
+
+#PBS -N s10_dif_summits
+#PBS -l nodes=1:ncpus=6
+#PBS -l walltime=00:30:00
+#PBS -q half_hour
+#PBS -m abe
+#PBS -M matthew.spriggs.452@cranfield.ac.uk
+
+# PBS directives and code that you should not change
+#===================================================
+#PBS -j oe
+#PBS -v "CUDA_VISIBLE_DEVICES="
+#PBS -W sandbox=PRIVATE
+#PBS -k n
+ln -s $PWD $PBS_O_WORKDIR/$PBS_JOBID
+## Change to working directory
+cd $PBS_O_WORKDIR
+## Calculate number of CPUs and GPUs
+export cpus=`cat $PBS_NODEFILE | wc -l`
+## Load production modules
+module use /apps2/modules/all
+## =============
+
 
 # Stop at runtime errors
 set -e
@@ -11,7 +37,7 @@ date
 echo ""
 
 # Folders
-base_folder="/mnt/beegfs/home/alexey.larionov/teaching_2024/epigenetics"
+base_folder="/mnt/beegfs/home/s430452/epigenetics"
 peaks_folder="${base_folder}/results/s07_peaks" # contains master_summits_bl.bed
 dif_peaks_folder="${base_folder}/results/s09_dif_peaks" # files with dif peaks
 dif_summits_folder="${base_folder}/results/s10_dif_summits" # for results
@@ -51,3 +77,9 @@ awk 'FNR==NR {a[$1]; next} $4 in a' \
 echo ""
 echo "Done"
 date
+
+# Clean-up (keep it at the end of the script)
+## ==========================================
+rm $PBS_O_WORKDIR/$PBS_JOBID
+
+
