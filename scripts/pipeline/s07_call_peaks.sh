aefac623fa879616d50c8b0b6d247fee315c2cbd
Matthew Spriggs <matspriggs@gmail.com>
2024-12-11 09:10:54 +0000

    updated s0y with PBS

--- scripts/pipeline/s07_call_peaks.sh
+++ scripts/pipeline/s07_call_peaks.sh
@@ -2,6 +2,33 @@
 # ATACseq analysis: calling peaks
 # Matthew Spriggs 11Dec2024
 
+
+# PBS directives that you should review and change if needed
+#-----------------------------------------------------------
+
+#PBS -N s07_call_peaks
+#PBS -l nodes=1:ncpus=6
+#PBS -l walltime=00:30:00
+#PBS -q half_hour
+#PBS -m abe
+#PBS -M matthew.spriggs.453@cranfield.ac.uk
+
+# PBS directives and code that you should not change
+#===================================================
+#PBS -j oe
+#PBS -v "CUDA_VISIBLE_DEVICES="
+#PBS -W sandbox=PRIVATE
+#PBS -k n
+ln -s $PWD $PBS_O_WORKDIR/$PBS_JOBID
+## Change to working directory
+cd $PBS_O_WORKDIR
+## Calculate number of CPUs and GPUs
+export cpus=`cat $PBS_NODEFILE | wc -l`
+## Load production modules
+module use /apps2/modules/all
+## =============
+
+
 # Stop at runtime errors
 set -e
 
@@ -64,3 +91,7 @@ awk 'FNR==NR {a[$4]; next} $4 in a' \
 echo ""
 echo "Done"
 date
+
+# Clean-up (keep it at the end of the script)
+## ==========================================
+rm $PBS_O_WORKDIR/$PBS_JOBID
